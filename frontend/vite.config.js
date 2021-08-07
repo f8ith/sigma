a414c7cdf8c471b6d1c86ee73a5b7b24810f9aa4
faith <jamienibtong@gmail.com>
2021-08-07 10:08:37 +0800

    added https

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,17 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import analyzer from 'rollup-plugin-analyzer'
+import fs from "fs"
+
+const options = {
+  key: fs.readFileSync('privkey.pem'),
+  cert: fs.readFileSync('fullchain.pem')
+};
 
 export default defineConfig({
+  server: {
+    https: options
+  },
   resolve: {
     alias: {
       'src': resolve(__dirname, 'src'),
